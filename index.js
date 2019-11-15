8ffddb92dfcecb298aea4448afaffdab334167f5
weichichou <weichi.cc@gmail.com>
2019-11-15 10:07:58 +0100

    set up router for sending message

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const app = express()
 const port = 4000
 
-app.listen(port, ()=>console.log(`Listening on port ${port}`))
+const messages = []
+
+app
+    .use(jsonParser)
+    .get('/', (req, res, next) => {
+        res.send('hello world')
+    })
+    .post('/message', (req, res, next)=> {
+        const {message} = req.body
+        messages.push(message)
+        res.send(message)
+    })
+    .listen(port, ()=>console.log(`Listening on port ${port}`))
