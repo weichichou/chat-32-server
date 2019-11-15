c2f138adc32e2cd93b9fecd7ef14e448a8d11e25
weichichou <weichi.cc@gmail.com>
2019-11-15 15:15:44 +0100

    Dictionary

--- index.js
+++ index.js
@@ -9,18 +9,48 @@ const corsMiddleware = cors()
 
 // stream is a list of clients connected
 const stream = new Sse()
-const messages = []
+
+const streams = {}
+
+const messages = {}
+// the object contains (key) room's name and (value) messages
+
+function send(data){
+    const string = JSON.stringify(data)
+    stream.send(string)
+}
 
 app
     .use(corsMiddleware)
     .use(jsonParser)
     .get('/stream', (req, res, next)=>{
-        const string = JSON.stringify(messages)
+        
+        const rooms = Object.keys(messages)
+        const string = JSON.stringify(rooms)
         
         stream.updateInit(string)
         
         //connect clients
         stream.init(req, res)
+    })
+    .get('/rooms/:roomName', (req, res, next)=>{
+        const {roomName} = req.params
+        const stream = streams[roomName]
+        const data = messages[roomName]
+        const string = JSON.stringify(data)
+        
+        stream.updateInit(string)
+        
+        //connect clients
+        stream.init(req, res)
+    })
+    .post('/room', (req, res, next)=>{
+        const {name} = req.body
+        send(name)
+
+        streams[name] = new Sse()
+
+        res.send(name)
     })
     .get('/', (req, res, next) => {
         res.send('hello world')
@@ -28,13 +58,15 @@ app
     .get('/message', (req, res, next)=>{
         res.send(messages)
     })
-    .post('/message', (req, res, next)=> {
+    .post('/message/:roomName', (req, res, next)=> {
         const {message} = req.body
-        
-        const string = JSON.stringify(message)
+        const {roomName} = req.params
+        const room = mesages[roomName]
+        room.push(message)
 
+        const stream = streams[roomName]
+        const string = JSON.stringify(message)
         stream.send(string)
-        messages.push(message)
         
         res.send(message)
     })
