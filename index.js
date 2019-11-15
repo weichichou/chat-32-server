3f4a5dbd024bc0da0fe2b76a40197f08fca6e6fa
weichichou <weichi.cc@gmail.com>
2019-11-15 10:38:33 +0100

    stream connection endpoint

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+const Sse = require('json-sse')
 const app = express()
 const port = 4000
 
+// stream is a list of clients connected
+const stream = new Sse()
 const messages = []
 
 app
     .use(jsonParser)
+    .get('/stream', (req, res, next)=>{
+        const string = JSON.stringify(messages)
+        
+        stream.updateInit(string)
+        
+        //connect clients
+        stream.init(req, res)
+    })
     .get('/', (req, res, next) => {
         res.send('hello world')
     })
